40405544c5e85b0964cd8a8ddb88c14e913706c5
Yash Rudra <rudrayash007@gmail.com>
2024-12-19 00:54:57 +0530

    modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ function App() {
               <textarea
                 id="message"
                 rows={8}
-                className="block p-2.5 w-full h-20 md:h-44 mt-2 md:mt-0 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block p-2.5 w-full h-20 md:h-44 mt-2 md:mt-0 text-sm rounded-lg border focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
                 placeholder="Write Your Queries here..."
                 onChange={(e) => setQuestion(e.target.value)}
               ></textarea>
